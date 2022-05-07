0a640a0276a2eaaf3b206ceb77f0f35ae9239ec5
Marcelo Farinha <marcelofarinha@live.com>
2022-05-06 23:51:32 -0300

    Add already build files to the new project repository

---
+++ src/main/java/dev/farinha/idmss/IdmssApplication.java
@@ -0,0 +1,13 @@
+package dev.farinha.idmss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdmssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdmssApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/farinha/idmss/IdmssApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.farinha.idmss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdmssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
